d44d2a11b380caa577a0fb553b67d9fab31cc490
sergiobaronsanz <s.baronsanz@gmail.com>
2025-09-17 10:54:57 +0200

    actualizado statics

--- polvosLom/settings.py
+++ polvosLom/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/sergio/proyectos/mi_proyecto/polvosLom/static/'
+STATIC_ROOT = '/home/sergio/proyectos/mi_proyecto/polvosLom/staticfiles/'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 
