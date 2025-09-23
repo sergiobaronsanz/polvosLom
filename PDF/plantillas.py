ec94cbdf3b649325f2e609680314ede73c0a9ae9
sergiobaronsanz <s.baronsanz@gmail.com>
2025-09-23 21:06:05 +0200

    Arreglada plantilla capa

--- PDF/plantillas.py
+++ PDF/plantillas.py
@@ -269,8 +269,8 @@ class PlantillasEnsayo():
             print(fila)
         for fila in resultados:
             valorIgnicion = fila.get_tipoIgnicion_display() or "-"
-            temperaturaMaxima = str(int(fila.tMaxima)) or "-"
-            tiempoTmax= str(int(fila.tiempoPrueba)) or "-"
+            temperaturaMaxima = str(int(fila.tMaxima)) if fila.tMaxima is not None else "-"
+            tiempoTmax= str(int(fila.tiempoPrueba)) if fila.tMaxima is not None else "-"
             
             
             self.pdf.cell(w=5, h= 8,border= "L", fill = 0)
